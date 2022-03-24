bad0c263aed14ba17135617e0fe7ea2af14723f3
Mamadou  NDIAYE <mamadou.ndiaye@gainde2000.sn>
2022-03-24 16:21:29 +0000

    Installation de bootstrap, jquery & fontawesome plus creation des composants login, inscription, menu, footer et dashboard enfin la configuration du routage ,ie les routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    component : LoginComponent
+  },
+  {
+    path : "register",
+    component : InscriptionComponent
+  },
+  {
+    path : "dashboard",
+    component : DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InscriptionComponent,
+    DashboardComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
